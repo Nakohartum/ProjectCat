7f689977ba9ae58aa1008ce88f8b414810386dba
Nakohartum <artemofgta@gmail.com>
2023-04-13 23:19:21 +0500

    Added dog and a patrol

---
+++ Assets/_Root/Scripts/Dog/DogController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UnityEngine;
+
+namespace _Root.Scripts.Dog
+{
+    [RequireComponent(typeof(DogView))]
+    public class DogController : MonoBehaviour
+    {
+        private DogView _dogView;
+        private float _speed;
+        private int _currentPoint = 0;
+        private float _stoppingDistance = 0.2f;
+
+        private void Awake()
+        {
+            _dogView = GetComponent<DogView>();
+            _speed = _dogView.Speed;
+        }
+
+        private void Start()
+        {
+            MovementLogic();
+        }
+
+        private void Update()
+        {
+            MovementLogic();
+        }
+
+        private void MovementLogic()
+        {
+            Debug.Log($"Current {_currentPoint}");
+            Debug.Log(Mathf.Abs(Mathf.Abs(_dogView.transform.position.x) - Mathf.Abs(_dogView.PatrolsPoint[_currentPoint].position.x)));
+            if (Mathf.Abs(Mathf.Abs(_dogView.transform.position.x) - Mathf.Abs(_dogView.PatrolsPoint[_currentPoint].position.x)) <= _stoppingDistance)
+            {
+                CalculateRoute();
+                _currentPoint = (_currentPoint + 1) % _dogView.PatrolsPoint.Length;
+            }
+            _dogView.MovementByVelocityEvent.CallMovementEvent(_speed);
+        }
+
+        private void CalculateRoute()
+        {
+            
+            if (Vector3.Dot(_dogView.transform.position, _dogView.PatrolsPoint[_currentPoint].position) > 0)
+            {
+                _speed *= -1;
+                _dogView.transform.localScale = new Vector3(-_dogView.transform.localScale.x, 1, 1);
+            }
+        }
+    }
+}
---
+++ Assets/_Root/Scripts/Dog/DogView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using _Root.Scripts.Movement;
+using UnityEngine;
+
+namespace _Root.Scripts.Dog
+{
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(MovementByVelocityEvent))]
+    [RequireComponent(typeof(MovementByVelocity))]
+    public class DogView : MonoBehaviour
+    {
+        [HideInInspector] public MovementByVelocityEvent MovementByVelocityEvent;
+        [field: SerializeField] public float Speed { get; private set; }
+        [field: SerializeField] public Transform[] PatrolsPoint { get; private set; }
+
+        private void Awake()
+        {
+            MovementByVelocityEvent = GetComponent<MovementByVelocityEvent>();
+        }
+    }
+}
